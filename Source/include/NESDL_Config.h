fe4170989ebe0e8cd6e7da0bd8deb86670bb5bb0
GDFauxtrot <gregduarte97@gmail.com>
2025-04-25 11:04:56 -0700

    Created config key table
      * Config value keys can be accessed via the ConfigKey class, containing constants for config file use
      * Added config key implementation to existing uses (last ROM and last log entries)
      * Created early versions of player input config keys, subject to change
        * Thoughts - no way to read data in sections, ini-style? I might add that functionality to SCL, or write my own entirely
        * It'd be way more useful to bind player inputs in per-player slots, with a controller type stored as well
        * Also great for organizing config information, I'm convincing myself that a barebones CFG is not the way to go
      * Converted some spaces to tabs

--- Source/include/NESDL_Config.h
+++ Source/include/NESDL_Config.h
@@ -1,15 +1,34 @@
 #pragma once
 
+// String consts relevant to our config file
+class ConfigKey
+{
+public:
+    constexpr static const char* LASTROM		= "last_rom";
+    constexpr static const char* LASTLOG		= "last_log";
+
+    constexpr static const char* INPUT_UP		= "input_up";
+    constexpr static const char* INPUT_DOWN		= "input_down";
+    constexpr static const char* INPUT_LEFT		= "input_left";
+    constexpr static const char* INPUT_RIGHT	= "input_right";
+    constexpr static const char* INPUT_A		= "input_a";
+    constexpr static const char* INPUT_B		= "input_b";
+    constexpr static const char* INPUT_SELECT	= "input_select";
+    constexpr static const char* INPUT_START	= "input_start";
+};
+
 class NESDL_Config
 {
 public:
-	void Init(NESDL_Core* c);
-	void WriteValue(string key, bool value);
-	void WriteValue(string key, int value);
-	void WriteValue(string key, string value);
-	bool ReadBool(const char* key, bool defVal = false);
-	int ReadInt(const char* key, int defVal = 0);
-	string ReadString(const char* key, string defVal = "");
+    constexpr static const char* FILENAME = "nesdl.cfg";
+
+    void Init(NESDL_Core* c);
+    void WriteValue(string key, bool value);
+    void WriteValue(string key, int value);
+    void WriteValue(string key, string value);
+    bool ReadBool(const char* key, bool defVal = false);
+    int ReadInt(const char* key, int defVal = 0);
+    string ReadString(const char* key, string defVal = "");
 private:
-	NESDL_Core* core;
+    NESDL_Core* core;
 };
--- Source/src/NESDL_Config.cpp
+++ Source/src/NESDL_Config.cpp
@@ -4,53 +4,53 @@ using namespace scl;
 
 void NESDL_Config::Init(NESDL_Core* c)
 {
-	core = c;
+    core = c;
 }
 
 void NESDL_Config::WriteValue(string key, bool value)
 {
-	config_file cfg(NESDL_CONFIG, config_file::WRITE);
-	cfg.put<bool>(key, value);
-	cfg.write_changes_unique();
-	cfg.close();
+    config_file cfg(FILENAME, config_file::WRITE);
+    cfg.put<bool>(key, value);
+    cfg.write_changes_unique();
+    cfg.close();
 }
 
 void NESDL_Config::WriteValue(string key, int value)
 {
-	config_file cfg(NESDL_CONFIG, config_file::WRITE);
-	cfg.put<int>(key, value);
-	cfg.write_changes_unique();
-	cfg.close();
+    config_file cfg(FILENAME, config_file::WRITE);
+    cfg.put<int>(key, value);
+    cfg.write_changes_unique();
+    cfg.close();
 }
 
 void NESDL_Config::WriteValue(string key, string value)
 {
-	config_file cfg(NESDL_CONFIG, config_file::WRITE);
-	cfg.put<string>(key, value);
-	cfg.write_changes_unique();
-	cfg.close();
+    config_file cfg(FILENAME, config_file::WRITE);
+    cfg.put<string>(key, value);
+    cfg.write_changes_unique();
+    cfg.close();
 }
 
 bool NESDL_Config::ReadBool(const char* key, bool defVal)
 {
-	config_file cfg(NESDL_CONFIG, config_file::READ);
-	bool result = cfg.get(key, defVal);
-	cfg.close();
-	return result;
+    config_file cfg(FILENAME, config_file::READ);
+    bool result = cfg.get(key, defVal);
+    cfg.close();
+    return result;
 }
 
 int NESDL_Config::ReadInt(const char* key, int defVal)
 {
-	config_file cfg(NESDL_CONFIG, config_file::READ);
-	int result = cfg.get(key, defVal);
-	cfg.close();
-	return result;
+    config_file cfg(FILENAME, config_file::READ);
+    int result = cfg.get(key, defVal);
+    cfg.close();
+    return result;
 }
 
 string NESDL_Config::ReadString(const char* key, string defVal)
 {
-	config_file cfg(NESDL_CONFIG, config_file::READ);
-	string result = cfg.get(key, defVal);
-	cfg.close();
-	return result;
+    config_file cfg(FILENAME, config_file::READ);
+    string result = cfg.get(key, defVal);
+    cfg.close();
+    return result;
 }
